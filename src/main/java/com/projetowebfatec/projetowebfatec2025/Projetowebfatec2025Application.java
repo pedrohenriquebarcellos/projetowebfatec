3533ba9a9121a8f45e5248121e88447953c2cbc7
pedrohenriquebacellos <pedrostyle@gmail.com>
2025-02-19 19:23:10 -0300

    :construction: feat: create java project structure

---
+++ src/main/java/com/projetowebfatec/projetowebfatec2025/Projetowebfatec2025Application.java
@@ -0,0 +1,13 @@
+package com.projetowebfatec.projetowebfatec2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projetowebfatec2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projetowebfatec2025Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetowebfatec/projetowebfatec2025/Projetowebfatec2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetowebfatec.projetowebfatec2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projetowebfatec2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
