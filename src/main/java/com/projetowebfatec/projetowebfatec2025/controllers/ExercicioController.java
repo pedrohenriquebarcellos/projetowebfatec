c47505b47b4b7db4b76d03092aa5450183019c1b
Pedro Barcellos <pedrostyle@gmail.com>
2025-02-20 22:45:48 -0300

    :construction: feat: Update controller with an empty route param example

--- src/main/java/com/projetowebfatec/projetowebfatec2025/controllers/ExercicioController.java
+++ src/main/java/com/projetowebfatec/projetowebfatec2025/controllers/ExercicioController.java
@@ -1,6 +1,9 @@
 package com.projetowebfatec.projetowebfatec2025.controllers;
 
+import java.util.Optional;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,6 +11,11 @@ public class ExercicioController {
 
     @GetMapping("")
     public String HelloWorld() {
-        return "Hello World";
+        return "No Param defined";
+    }
+
+    @GetMapping("{name}")
+    public String GetRouteName(@PathVariable Optional<String> name) {
+        return name.get();
     }
 }
