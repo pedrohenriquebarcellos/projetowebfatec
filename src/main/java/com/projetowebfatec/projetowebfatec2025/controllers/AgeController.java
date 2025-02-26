b84d524738b55593e52b13460e1a9e2c999828f5
Pedro Barcellos <pedrostyle@gmail.com>
2025-02-26 19:48:32 -0300

    :sparkles: feat: update age controller to age route

--- src/main/java/com/projetowebfatec/projetowebfatec2025/controllers/AgeController.java
+++ src/main/java/com/projetowebfatec/projetowebfatec2025/controllers/AgeController.java
@@ -2,6 +2,7 @@ package com.projetowebfatec.projetowebfatec2025.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
@@ -13,23 +14,27 @@ public class AgeController {
     private static final Integer ADULT = 19;
     private static final Integer OLD = 61;
     
-    @GetMapping("{age}")
-    public String getAge(@RequestParam String age) {
-        Integer currentAge = Integer.parseInt(age);
+    @GetMapping("/age/{age}")  
+    public String getAge(@PathVariable String age) {
+        try {
+            Integer currentAge = Integer.parseInt(age);
 
-        if (currentAge < CHILDREN) {
-            return "Criança";
-        } 
+            if (currentAge < CHILDREN) {
+                return "Criança";
+            } 
 
-        if (currentAge >= CHILDREN && currentAge <= TEENAGER) {
-            return "Adolescente";
+            if (currentAge >= CHILDREN && currentAge <= TEENAGER) {
+                return "Adolescente";
+            }
+
+            if (currentAge >= ADULT && currentAge < OLD) {
+                return "Adulto";
+            }
+
+            return "Idoso";
+        } catch (NumberFormatException e) {
+            return "Erro: O valor de 'age' não é um número válido!";
         }
-
-        if (currentAge > TEENAGER && currentAge < OLD) {
-            return "Adulto";
-        }
-        
-        return "Idoso";
     }
     
 }
