8657a705bc679acdb9926ee149ee2f23c81b0d3e
Pedro Barcellos <pedrostyle@gmail.com>
2025-02-27 21:42:52 -0300

    :sparkles: feat: Upload exercise one 27/02/25 Check if a number is even or odd

--- src/main/java/com/projetowebfatec/projetowebfatec2025/controllers/ExercicioController.java
+++ src/main/java/com/projetowebfatec/projetowebfatec2025/controllers/ExercicioController.java
@@ -44,4 +44,40 @@ public class ExercicioController {
             return "Erro: O valor de 'age' não é um número válido!";
         }
     }
+
+    /**
+     * Check if the received number is Odd or Even
+     * @param number
+     * @return String
+     */
+    @GetMapping("check-number/{number}")
+    public String CheckOddEvenNumber(@PathVariable String number) {
+        try {
+            if (!IsNumeric(number)) {
+                throw new NumberFormatException();
+            }
+
+            if (Integer.parseInt(number) % 2 == 0) {
+                return "Esse número é Par";
+            }
+
+            return "O número é impar";
+        } catch (Exception e) {
+            return "Precisa ser um número";
+        }
+    }
+
+    /**
+     * Verify if the param is a Number
+     * @param value
+     * @return Boolean
+     */
+    public Boolean IsNumeric(String value) {
+        try {
+            Double.parseDouble(value);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
 }
