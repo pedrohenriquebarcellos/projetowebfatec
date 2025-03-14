4faadc3b907474e501432023e8be4e508dadf994
Pedro Barcellos <pedrostyle@gmail.com>
2025-03-13 22:03:16 -0300

    :ambulance: feat: create delete and put method

--- src/main/java/com/projetowebfatec/projetowebfatec2025/controllers/ClienteController.java
+++ src/main/java/com/projetowebfatec/projetowebfatec2025/controllers/ClienteController.java
@@ -10,9 +10,11 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
@@ -33,9 +35,34 @@ public class ClienteController {
         return "O Cliente "+cliente.getNome()+ " de idade "+cliente.getIdade()+" foi criado";
     }
 
-    @PostMapping("/deletarCliente/{id}")
-    public void DeletarCliente(@PathVariable Long id) {
+    @DeleteMapping("/deletarCliente/{id}")
+    public String DeletarCliente(@PathVariable Long id) {
         clientes.removeIf(clientes -> clientes.getId().equals(id));
+        return "Cliente id:" + id + " removido com sucesso";
+    }
+
+    @PutMapping("/atualizarCliente/{id}")
+    public String AtualizarCliente(@RequestBody Cliente clienteAtualizado) {
+        for (Cliente cliente : clientes) {
+            if (cliente.getId().equals(clienteAtualizado.getId())) {
+
+                if (clienteAtualizado.getNome() != "") {
+                    cliente.setNome(clienteAtualizado.getNome());
+                }
+                
+                if (clienteAtualizado.getIdade() != null) {
+                    cliente.setIdade(clienteAtualizado.getIdade());
+                }
+                
+                if (clienteAtualizado.getEndereco() != "") {
+                    cliente.setEndereco(clienteAtualizado.getEndereco());
+                }
+
+                return "Ciente id:" + clienteAtualizado.getId() + " foi atualizado com sucesso";
+            }
+        }
+
+        return "Cliente não encontrado";
     }
 
     @GetMapping("/listarClientes")
